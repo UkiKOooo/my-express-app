6b6cd48eca3cf0e6019812a973b91225c8f445fe
UkiKOooo <120693556+UkiKOooo@users.noreply.github.com>
2026-04-07 21:49:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const cors = require('cors'); 
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());c
+  
+onst express = require('express');
 const { Pool } = require('pg');
 require('dotenv').config();
 
